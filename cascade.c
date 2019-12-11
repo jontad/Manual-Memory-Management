4871728b925e85e9624f9db3b6bcd1ff81080fae
jota5243 <jonathannoahtadese@gmail.com>
2019-12-11 14:53:09 +0100

    Improvement: Added global variable

--- cascade.c
+++ cascade.c
@@ -1,24 +1,14 @@
 #include <stdlib.h>
-#include <stdint.h>
 
-typedef struct allocate alloc_t;
-
-struct allocate
+size_t cascade_limit = 0;
+ 
+void set_cascade_limit(size_t limit)
 {
-  uint8_int ref_count;
-  obj *object;
-  function1_t destructor;
-  size_t cascade_limit;
-};
-
-  
-void set_cascade_limit(alloc_t *alloc, size_t limit)
-{
-  alloc->cascade_limit = limit;
+  cascade_limit = limit;
 }
 
 
-size_t get_cascade_limit(alloc_t *alloc)
+size_t get_cascade_limit()
 {
-  return alloc->cascade_limit;
+  return cascade_limit;
 }
