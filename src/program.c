638ca62c73e2b72d53cbf7282f4f8c1830c04e5a
mrbubblegum <daniel.ar.westberg@gmail.com>
2019-12-13 14:44:36 +0100

    added a small program for testing the garbage collector. It does not have its own destroy function at the moment

--- src/program.c
+++ src/program.c
@@ -4,8 +4,6 @@
 #include <stdbool.h>
 
 typedef struct person person_t;
-typedef struct link link_t;
-typedef struct list list_t;
 
 struct person
 {
@@ -13,19 +11,6 @@ struct person
   char *last_name;
   int age;
 };
-
-struct link
-{
-  person_t *current;
-  link_t *next;
-};
-
-struct list
-{
-  link_t *first;
-  link_t *last;
-};
-
 
 person_t *person_create(char *first_name, char *last_name, int age)
 {
@@ -36,35 +21,15 @@ person_t *person_create(char *first_name, char *last_name, int age)
   return new_person;
 }
 
-link_t *link_create(person_t *current, link_t *next)
-{
-  link_t *new_link = calloc(1, sizeof(link_t));
-  new_link->current = current;
-  new_link->next = next;
-  return new_link;
-}
-
-list_t *list_create(link_t *first, link_t *last)
-{
-  list_t *list = calloc(1, sizeof(list_t));
-  list->first = first;
-  list->last = last;
-  return list;
-}
-
-list_t *cray_party()
+void cray_party()
 {
   person_t *olle = person_create("Olle", "Olsson", 99);
   person_t *bosse = person_create("Bosse", "Bossman", 56);
+  person_t *kalle = person_create("Kalle", "Anka", 123);
   person_t *kaffe = person_create("Lindwall", "Zoegas", 105);
-  link_t *last = link_create(kaffe, NULL);
-  link_t *second = link_create(bosse, last);
-  link_t *first = link_create(olle, second);
-  list_t *list = list_create(first, last);
-  return list;
 }
 
 int main()
 {
-  list_t *party = cray_party();
+  cray_party();
 }
