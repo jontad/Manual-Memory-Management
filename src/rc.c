7be0be2fc6a8fd40b95ac26c3db150ab66975eb3
Elias Insulander <eliasinsulander@student-233-155.eduroam.uu.se>
2019-12-16 11:59:24 +0100

    retain, release, rc (med dokumentation)

--- src/rc.c
+++ src/rc.c
@@ -5,6 +5,13 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * @file rc.c
+ * @author Elias Insulander & Alex Kangas
+ * @date 16 Dec 2019
+ * @brief Gets reference count of an object, adds and subtracts to it.
+ */
+
 typedef struct allocate alloc_t;
 
 struct allocate
@@ -14,19 +21,25 @@ struct allocate
 };
 
 
+
+/// @brief adds 1 to reference count of an object
+/// @param object object to increase reference count of
 void rc_add_one(obj *object)
 {
   obj *tmp = object-sizeof(function1_t)-1;
   *(uint8_t *)tmp = *(uint8_t *)tmp + 1;
 }
 
+/// @brief subtracts 1 from reference count of an object
+/// @param object object to decrease reference count of
 void rc_sub_one(obj *object)
 {
   obj *tmp = object-sizeof(function1_t)-1;
   *(uint8_t *)tmp = *(uint8_t *)tmp - 1;
 }
 
-
+/// @brief adds 1 to reference count if object exists
+/// @param object object to increase reference count of
 void retain(obj *object)
 {
   if (object == NULL)
@@ -39,7 +52,8 @@ void retain(obj *object)
     }
 }
 
-
+/// @brief subtracts 1 to reference count if object exists, destroys object if reference count is 1
+/// @param object object to decrease reference count of
 void release(obj *object)
 {
   if (object == NULL)
@@ -57,7 +71,9 @@ void release(obj *object)
     }
 }
 
-
+/// @brief gets reference count of an object
+/// @param object object to get reference count of
+/// @return reference count
 size_t rc(obj *object)
 {
   obj *tmp = object-sizeof(function1_t)-1;
