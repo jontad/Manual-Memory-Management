3093dc8acfd566c8618709c604ccc5c67395251c
mrbubblegum <daniel.ar.westberg@gmail.com>
2019-12-16 13:24:37 +0100

    program now uses our allocate function

--- src/program.c
+++ src/program.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include "refmem.h"
 
 typedef struct person person_t;
 
@@ -12,18 +13,18 @@ struct person
   int age;
 };
 
+void person_kill(obj *person)
+{
+  free(person);
+}
+
 person_t *person_create(char *first_name, char *last_name, int age)
 {
-  person_t *new_person = calloc(1, sizeof(person_t));
+  person_t *new_person = allocate(sizeof(person_t), person_kill);
   new_person->first_name = first_name;
   new_person->last_name = last_name;
   new_person->age = age;
   return new_person;
-}
-
-void person_kill(person_t *person)
-{
-  free(person);
 }
 
 void cray_party()
@@ -31,7 +32,7 @@ void cray_party()
   person_t *olle = person_create("Olle", "Olsson", 99);
   person_t *bosse = person_create("Bosse", "Bossman", 56);
   person_t *kalle = person_create("Kalle", "Anka", 123);
-  person_t *kaffe = person_create("Lindwall", "Zoegas", 105);
+  person_t *kaffe = person_create("Zoegas", "Lindwall", 105);
 }
 
 int main()
