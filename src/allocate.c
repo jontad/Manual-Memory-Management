8366bed831a0cce1195acb15e5f7174bc6733b8e
Robert <robert.paananen@hotmail.com>
2019-12-17 12:48:51 +0100

    Fixed so that all warnings disappeared

--- src/allocate.c
+++ src/allocate.c
@@ -16,8 +16,8 @@ obj *allocate(size_t bytes, function1_t destructor)
   */
   obj *alloc = malloc(1 + sizeof(function1_t) + bytes);
   memset(alloc,0,1);
-  memcpy(alloc+1,&destructor,sizeof(destructor));
-  alloc += sizeof(destructor) + 1;
+  memcpy((obj *)((char *)alloc+1),&destructor,sizeof(destructor));
+  alloc = (obj *)((char *)alloc + sizeof(destructor) + 1);
   ioopm_list_t *list = linked_list_get();
   if (list) ioopm_linked_list_append(list, (elem_t){.obj_val = alloc});
   return alloc;
@@ -29,8 +29,8 @@ obj *allocate_array(size_t elements, size_t bytes, function1_t destructor)
 {
   obj *alloc = malloc(1 + sizeof(function1_t) + elements*bytes);
   memset(alloc,0,1);
-  memcpy(alloc+1,&destructor,sizeof(destructor));
-  alloc += sizeof(destructor) + 1;
+  memcpy((obj *)((char *)alloc+1),&destructor,sizeof(destructor));
+  alloc = (obj *)((char *)alloc + sizeof(destructor) + 1);
   memset(alloc,0,elements*bytes);
   ioopm_list_t *list = linked_list_get();
   if (list) ioopm_linked_list_append(list, (elem_t){.obj_val = alloc});
@@ -39,8 +39,8 @@ obj *allocate_array(size_t elements, size_t bytes, function1_t destructor)
 
 void deallocate_aux(obj *object)
 {
-  obj *tmp = object-sizeof(function1_t)-1;
-  function1_t destructor = *(function1_t *)(tmp+1); 
+  obj *tmp = (obj *)((char *)object-sizeof(function1_t)-1);
+  function1_t destructor = *(function1_t *)((obj *)((char *)tmp+1)); 
   if(destructor)
     {
       destructor(object);
@@ -57,7 +57,7 @@ void retain(obj *object)
 {
   if(object)
     {
-      obj *tmp = object-sizeof(function1_t)-1;
+      obj *tmp = (obj *)((char *)object-sizeof(function1_t)-1);
       uint8_t ref_count = *(uint8_t *)tmp;
       ref_count++;
       memset(tmp,ref_count,1);
@@ -68,7 +68,7 @@ void release(obj *object)
 {
   if(object)
     {
-      obj *tmp = object-sizeof(function1_t)-1;
+      obj *tmp = (obj *)((char *)object-sizeof(function1_t)-1);;
       uint8_t ref_count = *(uint8_t *)tmp;
       ref_count--;
       memset(tmp,ref_count,1);
@@ -78,7 +78,7 @@ void release(obj *object)
 
 size_t rc(obj *object)
 {
-  obj *tmp = object-sizeof(function1_t)-1;
+  obj *tmp = (obj *)((char *)object-sizeof(function1_t)-1);
   uint8_t ref_count = *(uint8_t *)tmp;
   return ref_count;
 }
